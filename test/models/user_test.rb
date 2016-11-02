acacf17a7444293bb4b6db9ad99026421c446dfb
etschmidt <Ethan.T.Schmidt@gmail.com>
2016-11-02 17:06:49 +0000

    Add user following

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -85,20 +85,19 @@ class UserTest < ActiveSupport::TestCase
 
   test "feed should have the right posts" do
     michael = users(:michael)
-    archer = users(:archer)
-    lana = users(:lana)
-    #posts from followed user
+    archer  = users(:archer)
+    lana    = users(:lana)
+    # Posts from followed user
     lana.microposts.each do |post_following|
       assert michael.feed.include?(post_following)
     end
-    #post from self
-    michael.microposts.each do |posts-self|
+    # Posts from self
+    michael.microposts.each do |post_self|
       assert michael.feed.include?(post_self)
     end
-    #posts from unfollowed
-    archer.microposts.each do |post_unfollowed| 
+    # Posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
       assert_not michael.feed.include?(post_unfollowed)
     end
   end
-
 end
